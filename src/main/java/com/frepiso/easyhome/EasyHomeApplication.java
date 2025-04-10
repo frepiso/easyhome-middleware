afd63d79b882165392c649cb39770e64d728f0e7
frepiso <38246328+frepiso@users.noreply.github.com>
2025-04-10 13:07:54 +0200

    chore: main scaffolding

---
+++ src/main/java/com/frepiso/easyhome/EasyHomeApplication.java
@@ -0,0 +1,13 @@
+package com.frepiso.easyhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyHomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/frepiso/easyhome/EasyHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frepiso.easyhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
